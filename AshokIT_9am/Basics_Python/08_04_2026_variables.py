86725d9b6ae584c3072ea47ea65514e2fd691d24
aditya-genai <urwithaditya703@gmail.com>
2026-04-06 21:18:30 -0700

    code modified

---
+++ AshokIT_9am/Basics_Python/08_04_2026_variables.py
@@ -0,0 +1,15 @@
+# # num1 =100
+# # num2=200
+# # addition = num1+num2
+# # print("addition is", addition)
+# # print (type(addition))
+# str = "Hello World" # string is a sequence of characters and immutable
+# print(str)
+# print (str[0])
+# print (str[:5])
+# str[0]=p
+str="Python"
+str1="programming"
+print(str+" "+str1) # concatenation
+print("Py" in str) # membership operator
+print("py" in str) # case sensitive
