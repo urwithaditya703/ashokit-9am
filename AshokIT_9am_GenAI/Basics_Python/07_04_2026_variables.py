c6bf1f1cfa2f7cc2ea3a8c524e3ea7499a2ff1fd
aditya-genai <urwithaditya703@gmail.com>
2026-04-09 21:31:09 -0700

    code on 04_10_2026

--- AshokIT_9am_GenAI/Basics_Python/07_04_2026_variables.py
+++ AshokIT_9am_GenAI/Basics_Python/07_04_2026_variables.py
@@ -1,157 +1,157 @@
-# string
-# collection of characters
-# "" (double quotes) ,  '' (single quotes) ,  """ """(triple quotes)
-# str1 = "Hello,Python"
-# str2 = 'Hello, Python Variables !!!'
-# str3 = """
-#         Hello Team,
-#             Welcome to Python
-#             Time : 9AM (IST)
-# """
-# print(str1)
-# print(str2)
-# print(str3)
+# # # string
+# # # collection of characters
+# # # "" (double quotes) ,  '' (single quotes) ,  """ """(triple quotes)
+# # # str1 = "Hello,Python"
+# # # str2 = 'Hello, Python Variables !!!'
+# # # str3 = """
+# # #         Hello Team,
+# # #             Welcome to Python
+# # #             Time : 9AM (IST)
+# # # """
+# # # print(str1)
+# # # print(str2)
+# # # print(str3)
 
-# """
-#     this is ignored line
-# """
-# x = 100
-# print(x)
+# # # """
+# # #     this is ignored line
+# # # """
+# # # x = 100
+# # # print(x)
 
-# str = "Hello"
-# print(str[0], str[-1])
-# print(str[-1])
-# print(str[0:2])
-# print(str[:2])
-# print(str[2:])
-# print(str[::-1])
+# # # str = "Hello"
+# # # print(str[0], str[-1])
+# # # print(str[-1])
+# # # print(str[0:2])
+# # # print(str[:2])
+# # # print(str[2:])
+# # # print(str[::-1])
 
-# str = "Python" # immutable
-# str[0] = "H"
-
-
-# str = "Python"
-# str1 = "p" + str[1:]
-# print(str1)
-
-# str1 = "Hello"
-# str2 = "World"
-# print(str1 +" " +str2)
-# print(str1 * 3)
-
-# print("Py" in "Python")
-# print("Java" not in "Python")
-
-# print( len("Python") )                      # know number of characters
-# print( "HELLO".lower() )                    # converts Uppercase to Lowercase
-# print( "hello".upper() )                    # converts lowercase to uppercase
-# print( "hello".replace("h","y") )           # perform replacement
-# print("welcome to python".split(" "))       # perform split() based on special character
-# print(",".join(['welcome', 'to', 'python']).replace(",", " ")) # join
-
-# str = "Python"
-# for ch in str:
-#     print(ch)
-
-# str = "DataSciencs"
-# msg = f"welcome to {str}"
-# print(msg) 
-
-# print("Hello\nWorld") #\n - new line
-# print("Hello\tWorld") #\t - tab space
-# print("Welcome to \"DataScience\"") # include double quotes
-# print("it\'s a DataSciencs") # include '
-# print("C:\\User\\Python") # include \
-
-# str = "python"
-# new_str= str.replace("p","y")
-# print(new_str)
-# print(str)
-
-# Boolean
-# True -- 1
-# False -- 0
-# flag1 = True
-# print(flag1)
-# flag2 = False
-# print(flag2)
-# print(True + 1)
-# print(True + False)
-
-age = 18
-# if age >=18:
-#     print("Major !!!")
-# else:
-#     print("Minor !!!")
-
-# print( "Major" if age>18 else "Minor" )
-
-# None --> No Value / Nothing / "Empty but not zero"
-# x = None
-# print(x)
-# print(type(x))
-
-# list
-# collection of elements
-# []
-# Hetrogeneous
-# Duplicates
-# Ordered
-# mutable
-
-# list1 = [10,10,"hello",True,10.1,None]
-# print(list1)
-# list2 = [10,20,30,40,50]
-# print(list2[0])
-# print(list2[-1])
-# list2[0] = 1000
-# print(list2)
-# print(type(list2))
-
-# tuple
-# collection of elements
-# ()
-# Hetrogeneous
-# Duplicates
-# Ordered
-# immutable
-# t1 = (10,10,20,30,10,True,"Hello",10.1,None)
-# print(type(t1))
-# t1[0] = 1000
-
-# dict
-# key & value 
-# d1 = {
-#     "name" : "DataScience",
-#     "sub" : "Gen AI",
-#     "cloud" : "MLops, LLOps"
-# }
-# print(d1["name"])
-# d1["sub"] = "Gen AI & Agentic AI"
-# del d1["cloud"]
-# print(d1)
-# print(type(d1))
+# # # str = "Python" # immutable
+# # # str[0] = "H"
 
 
-# Set
-# collection of elements
-# {}
-# no duplicates
-# no order
-# s1 = {1,1,2,3,1,2,3,None,None}
-# print(len(s1))
-# print(s1[0])
-# print(type(s1))
+# # # str = "Python"
+# # # str1 = "p" + str[1:]
+# # # print(str1)
 
-# int
-# float
-# str
-# bool
-# list
-# tuple
-# dict
-# None
-# set
+# # # str1 = "Hello"
+# # # str2 = "World"
+# # # print(str1 +" " +str2)
+# # # print(str1 * 3)
+
+# # # print("Py" in "Python")
+# # # print("Java" not in "Python")
+
+# # # print( len("Python") )                      # know number of characters
+# # # print( "HELLO".lower() )                    # converts Uppercase to Lowercase
+# # # print( "hello".upper() )                    # converts lowercase to uppercase
+# # # print( "hello".replace("h","y") )           # perform replacement
+# # # print("welcome to python".split(" "))       # perform split() based on special character
+# # # print(",".join(['welcome', 'to', 'python']).replace(",", " ")) # join
+
+# # # str = "Python"
+# # # for ch in str:
+# # #     print(ch)
+
+# # # str = "DataSciencs"
+# # # msg = f"welcome to {str}"
+# # # print(msg) 
+
+# # # print("Hello\nWorld") #\n - new line
+# # # print("Hello\tWorld") #\t - tab space
+# # # print("Welcome to \"DataScience\"") # include double quotes
+# # # print("it\'s a DataSciencs") # include '
+# # # print("C:\\User\\Python") # include \
+
+# # # str = "python"
+# # # new_str= str.replace("p","y")
+# # # print(new_str)
+# # # print(str)
+
+# # # Boolean
+# # # True -- 1
+# # # False -- 0
+# # # flag1 = True
+# # # print(flag1)
+# # # flag2 = False
+# # # print(flag2)
+# # # print(True + 1)
+# # # print(True + False)
+
+# # age = 18
+# # # if age >=18:
+# # #     print("Major !!!")
+# # # else:
+# # #     print("Minor !!!")
+
+# # # print( "Major" if age>18 else "Minor" )
+
+# # # None --> No Value / Nothing / "Empty but not zero"
+# # # x = None
+# # # print(x)
+# # # print(type(x))
+
+# # # list
+# # # collection of elements
+# # # []
+# # # Hetrogeneous
+# # # Duplicates
+# # # Ordered
+# # # mutable
+
+# # # list1 = [10,10,"hello",True,10.1,None]
+# # # print(list1)
+# # # list2 = [10,20,30,40,50]
+# # # print(list2[0])
+# # # print(list2[-1])
+# # # list2[0] = 1000
+# # # print(list2)
+# # # print(type(list2))
+
+# # # tuple
+# # # collection of elements
+# # # ()
+# # # Hetrogeneous
+# # # Duplicates
+# # # Ordered
+# # # immutable
+# # # t1 = (10,10,20,30,10,True,"Hello",10.1,None)
+# # # print(type(t1))
+# # # t1[0] = 1000
+
+# # # dict
+# # # key & value 
+# # # d1 = {
+# # #     "name" : "DataScience",
+# # #     "sub" : "Gen AI",
+# # #     "cloud" : "MLops, LLOps"
+# # # }
+# # # print(d1["name"])
+# # # d1["sub"] = "Gen AI & Agentic AI"
+# # # del d1["cloud"]
+# # # print(d1)
+# # # print(type(d1))
+
+
+# # # Set
+# # # collection of elements
+# # # {}
+# # # no duplicates
+# # # no order
+# # # s1 = {1,1,2,3,1,2,3,None,None}
+# # # print(len(s1))
+# # # print(s1[0])
+# # # print(type(s1))
+
+# # # int
+# # # float
+# # # str
+# # # bool
+# # # list
+# # # tuple
+# # # dict
+# # # None
+# # # set
 
 
 # num1 = 200
@@ -159,10 +159,10 @@ age = 18
 
 # addition = num1 + num2
 # print(addition)
-# print(type(addition)) # int # type() is the predefined func, used to know the datatype
+# # # print(type(addition)) # int # type() is the predefined func, used to know the datatype
 
-# num3 = 10.5
-# num4 = -10.5
-# res1 = num3 + num4
-# print(res1)
-# print(type(res1)) # float
+# # # num3 = 10.5
+# # # num4 = -10.5
+# # # res1 = num3 + num4
+# # # print(res1)
+# # # print(type(res1)) # float
---
+++ AshokIT_9am_GenAI/Basics_Python/10_04_2026.py
@@ -0,0 +1,99 @@
+# # # # # # # # def function1():
+# # # # # # # #     print("This is function 1")
+# # # # # # # # function1()
+# # # # # # # def add(a, b):
+# # # # # # #     result = a + b
+# # # # # # #     print("The sum is:", result)
+# # # # # # # add(10, 20)
+# # # # # # def sutraction():
+# # # # # #     a = 20
+# # # # # #     b = 10
+# # # # # #     result = a - b
+# # # # # #     print("The sutraction is:", result)
+# # # # # # sutraction()
+# # # # # def multiplication(a, b):
+# # # # #     result = a * b
+# # # # #     return result
+# # # # # x=multiplication(10, 20)
+# # # # # print(f"The multiplication is: {x}")
+# # # # def division(a, b):
+# # # #     if b != 0:
+# # # #         result = a / b
+# # # #         return result
+# # # #     else:
+# # # #         return "Error: Division by zero is not allowed."
+# # # # final_result = division(10, 2)
+# # # # print(f"The division is: {final_result}")
+# # # def db_connection(username, password, db_name):
+# # #     if db_name == "my_database" and username == "admin" and password == "password123":
+# # #         return "Database Connection Established"
+# # #     else:
+# # #         return "Error: Invalid credentials or database name."
+# # # connection_status = db_connection("admin", "password123", "my_database")
+# # # print(connection_status)
+# # #default arguments in function
+# # def greet(name="Guest"):
+# #     print(f"Hello, {name}!")
+# # greet()  # Output: Hello, Guest!
+# # greet("Alice")  # Output: Hello, Alice!
+# # greet(100)  # Output: Hello, 100!
+
+# def func_one(num1, num2):
+#     print(num1, num2)
+# func_one(num2=10, num1=20)
+
+#data stored in tuple is immutable
+my_tuple = (1, 2, 3, 4, 5)
+print(my_tuple[0])  # Output: 1
+print(my_tuple[2])  # Output: 3 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
