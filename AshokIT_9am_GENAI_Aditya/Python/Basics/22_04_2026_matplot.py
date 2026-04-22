7c97d1f8347862ff49a89c745eae080dd593e8f8
aditya-genai <urwithaditya703@gmail.com>
2026-04-21 21:56:56 -0700

    code commit on 22_04-2026

---
+++ AshokIT_9am_GENAI_Aditya/Python/Basics/22_04_2026_matplot.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+x=[1,2,3]
+y=[10,20,30]
+plt.plot(x, y, color="red", linestyle="--")
+plt.xlabel("XAxis Value")
+plt.ylabel("YAxis Value")
+plt.title("Line Plot")
+plt.show()
+# x=["std11","std2"]
